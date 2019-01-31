011a1c98544555e46be557692bfe879de54ab4f4
Pavol Srna <psrna@redhat.com>
2019-01-31 17:45:34 +0100

    Remove CSS

--- ui/reporter.js
+++ ui/reporter.js
@@ -5,12 +5,11 @@ const Mustache = require('mustache');
 const {Repository, Commit} = require('../repository.js');
 
 let template = fs.readFileSync('./ui/templates/report.mst');
-let css = fs.readFileSync('node_modules/bootstrap/dist/css/bootstrap.min.css')
 
 
 function generateReport(repos){
 
-    let output = Mustache.render(template.toString(), Object.assign({}, {css}, {repos}));
+    let output = Mustache.render(template.toString(), {repos});
     fs.writeFile("./report.html", output, function(err) {
         if(err) {
             return console.log(err);
